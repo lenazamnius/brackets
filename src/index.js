48c0fdf0676c61773ca8ff282f56f23cdab9e17a
Lena Zamnius <lena.zamnius@gmail.com>
2020-03-04 11:51:21 +0200

    feat: upload brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 != 0){
+        return false;
+    }
+    let i = 0;
+    let j = str.length;
+    while (i < j) {
+        if (str.includes("()")) {
+            str = str.replace("()", "");
+        }
+        if (str.includes("{}")) {
+            str = str.replace("{}", "");
+        }
+        if (str.includes("[]")) {
+            str = str.replace("[]", "");
+        }
+        if (str.includes("||")) {
+            str = str.replace("||", "");
+        }
+        if (str.includes("12")) {
+            str = str.replace("12", "");
+        }
+        if (str.includes("34")) {
+            str = str.replace("34", "");
+        }
+        if (str.includes("56")) {
+            str = str.replace("56", "");
+        }
+        if (str.includes("77")) {
+            str = str.replace("77", "");
+        }
+        if (str.includes("88")) {
+            str = str.replace("88", "");
+        }
+        i++
+    }
+    return str == "";
 }
